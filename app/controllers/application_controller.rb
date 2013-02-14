7deda5bcaad9a68980b67b4a7e2e31012df82846
Michael May <mmay@homeaway.com>
2013-02-13 20:35:38 -0600

    fuckin' devise, changing requirements and shit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  respond_to :json
+
   protect_from_forgery
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,9 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :name, :profile_picture, :email, :field_of_study, :degree_program, :participant_role, :university, :participant_month, :participant_year, :linkedin, :portfolio, :participant_team, :company
+  attr_accessible :name, :profile_picture, :field_of_study, :degree_program, :participant_role, :university, :participant_month, :participant_year, :linkedin, :portfolio, :participant_team, :company, :twitter
+
+  validates_presence_of :email
+  validates_presence_of :name
+
 end
